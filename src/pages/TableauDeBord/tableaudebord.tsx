e5d670e768d74038da0079fdbd141f0cb5ad28db
Hugo Le Galle <hugo@Ordinateur-portable-de-Hugo.local>
2024-05-18 12:57:44 +0200

    commit

--- src/pages/TableauDeBord/tableaudebord.tsx
+++ src/pages/TableauDeBord/tableaudebord.tsx
@@ -1,49 +1,74 @@
+import { Link } from "react-router-dom"
+
 export default function TableauDeBord() {
+
   return <>
     <h2 className="text-5xl font-thin mb-9">Tableau de bord</h2>
     <section className=" flex flex-col gap-4">
 
-      <div className="flex flex-row gap-4  w-full text-left">
-        <div className="flex flex-col-reverse justify-between w-full bg-zinc-900 rounded-2xl hover:bg-opacity-80 transition-all p-2 gap-4">
-          <p>Dépense</p>
-          <p className="text-4xl">1 000.00€</p>
-        </div>
-        <div className="flex flex-col-reverse justify-between w-full bg-zinc-900 rounded-2xl hover:bg-opacity-80 transition-all p-2 gap-4">
-          <p>Recette</p>
-          <p className="text-4xl">1 000.00€</p>
-        </div>
-        <div className="flex flex-col-reverse justify-between w-full bg-zinc-900 rounded-2xl hover:bg-opacity-80 transition-all p-2 gap-4">
-          <p>Moyenne dépense</p>
-          <p className="text-4xl">1 000.00€</p>
-        </div>
-        <div className="flex flex-col-reverse justify-between w-full bg-zinc-900 rounded-2xl hover:bg-opacity-80 transition-all p-2 gap-4">
-          <p>Moyenne recette</p>
-          <p className="text-4xl">1 000.00€</p>
-        </div>
-      </div >
 
-      <div className="flex flex-row-reverse w-full h-64 gap-4">
-        <div className="bg-zinc-900 h-full w-3/5 rounded-2xl hover:bg-opacity-80 transition-all p-2">
-
-        </div>
-        <div className="flex flex-col h-full w-2/5 gap-4">
-          <div className="bg-zinc-900 w-full h-1/2 rounded-2xl hover:bg-opacity-80 transition-all p-2">
-
-          </div>
-          <div className="bg-zinc-900 w-full h-1/2 rounded-2xl hover:bg-opacity-80 transition-all p-2">
-
-          </div>
-        </div>
-      </div>
 
       <div className="flex flex-row gap-4  w-full">
-        <div className="w-full h-44 bg-zinc-900 rounded-2xl hover:bg-opacity-80 transition-all p-2">
-          <p>Moyenne recette</p>
-          <p className="text-4xl">1 000.00€</p>
+        <div className="flex flex-col justify-between items-center w-full h-80 bg-zinc-900 rounded-2xl transition-all p-2">
+          <h2 className="text-2xl">Dépenses</h2>
+          <div className="flex flex-row justify-evenly items-center w-full">
+            <div className="w-1/3 rounded-xl bg-zinc-800 p-2 gap-2 flex flex-col">
+              CAMEMBERT
+
+            </div>
+            <div className="w-1/3 rounded-xl bg-zinc-800 p-2 gap-2 flex flex-col">
+              <p>* </p>
+              <p>* </p>
+              <p>* </p>
+            </div>
+          </div>
+          <Link to="/depense" className="bg-zinc-700 w-1/2 py-2 rounded-xl transition-all border-2 border-transparent hover:border-blue-600">Voir mes dépenses</Link>
         </div>
-        <div className="w-5/6 h-44 bg-zinc-900 rounded-2xl hover:bg-opacity-80 transition-all p-2">
-          <p>Moyenne recette</p>
-          <p className="text-4xl">1 000.00€</p>
+        <div className="flex flex-col justify-between items-center w-full h-80 bg-zinc-900 rounded-2xl transition-all p-2">
+          <h2 className="text-2xl">Recettes</h2>
+          <div className="flex flex-row justify-evenly items-center w-full">
+            <div className="w-1/3 rounded-xl bg-zinc-800 p-2 gap-2 flex flex-col">
+              CAMEMBERT
+
+            </div>
+            <div className="w-1/3 rounded-xl bg-zinc-800 p-2 gap-2 flex flex-col">
+              <p>* </p>
+              <p>* </p>
+              <p>* </p>
+            </div>
+          </div>
+          <Link to="/recette" className="bg-zinc-700 w-1/2 py-2 rounded-xl transition-all border-2 border-transparent hover:border-blue-600">Voir mes recettes</Link>
+        </div>
+      </div>
+      <div className="flex flex-row gap-4  w-full">
+        <div className="flex flex-col justify-between items-center w-full h-80 bg-zinc-900 rounded-2xl transition-all p-2">
+          <h2 className="text-2xl">Épargnes</h2>
+          <div className="flex flex-row justify-evenly w-full">
+            <div className="flex flex-col items-center w-2/5 rounded-xl p-2 bg-zinc-800">
+              <h2 className="text-xl">Livret A</h2>
+              <hr className="w-1/4" />
+              <p>10 000.00 €</p>
+            </div>
+            <div className="flex flex-col items-center w-2/5 rounded-xl p-2 bg-zinc-800">
+              <h2 className="text-xl">LEP</h2>
+              <hr className="w-1/4" />
+              <p>10 000.00 €</p>
+            </div>
+          </div>
+          <Link to="/epargn" className="bg-zinc-700 w-1/2 py-2 rounded-xl transition-all border-2 border-transparent hover:border-blue-600">Voir mon épargne</Link>
+        </div>
+        <div className="flex flex-col justify-between items-center w-full h-80 bg-zinc-900 rounded-2xl transition-all p-2">
+          <h2 className="text-2xl">Investissements</h2>
+          <div className="flex flex-row justify-evenly w-full">
+            <div className="w-1/3 rounded-xl bg-zinc-800 p-2">
+              <h2 className="text-xl">En cours</h2>
+            </div>
+            <div className="w-1/3 rounded-xl bg-zinc-800 p-2">
+              <h2 className="text-xl">Rendements</h2>
+              <p>4%</p>
+            </div>
+          </div>
+          <Link to="/invest" className="bg-zinc-700 w-1/2 py-2 rounded-xl transition-all border-2 border-transparent hover:border-blue-600">Voir mes investissements</Link>
         </div>
       </div>
 
