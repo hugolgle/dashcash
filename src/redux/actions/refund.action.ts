b2e7a6111ee3e97d75f3cefbe641986ab4b6d9ac
Hugo Le Galle <hugo@Ordinateur-portable-de-Hugo.local>
2024-05-31 21:17:11 +0200

    backend refund

--- src/redux/actions/refund.action.ts
+++ src/redux/actions/refund.action.ts
@@ -4,8 +4,8 @@ import axios from 'axios';
 import { GET_TRANSACTIONS } from './transaction.action';
 
 export const ADD_REFUND = "ADD_REFUND";
-// export const EDIT_REFUND = "EDIT_REFUND";
-// export const DELETE_REFUND = "DELETE_REFUND";
+export const EDIT_REFUND = "EDIT_REFUND";
+export const DELETE_REFUND = "DELETE_REFUND";
 
 // Action pour ajouter un remboursement
 export const addRefund = (transactionId: any, refundData: any) => {
@@ -24,28 +24,28 @@ export const addRefund = (transactionId: any, refundData: any) => {
 };
 
 
-// // Action pour modifier un remboursement
-// export const editRefund = (transactionId: any, refundData: any) => {
-//     return async (dispatch: any) => {
-//         try {
-//             await axios.put(`http://localhost:5001/transactions/${transactionId}/refund/${refundData.id}`, refundData);
-//             dispatch({ type: EDIT_REFUND, payload: refundData });
-//             dispatch({ type: GET_TRANSACTIONS }); // Rafraîchir les transactions après la modification du remboursement
-//         } catch (error) {
-//             throw error;
-//         }
-//     };
-// };
+// Action pour modifier un remboursement
+export const editRefund = (transactionId: any, refundData: any) => {
+    return async (dispatch: any) => {
+        try {
+            await axios.put(`http://localhost:5001/transactions/${transactionId}/refund/${refundData.id}`, refundData);
+            dispatch({ type: EDIT_REFUND, payload: refundData });
+            dispatch({ type: GET_TRANSACTIONS }); // Rafraîchir les transactions après la modification du remboursement
+        } catch (error) {
+            throw error;
+        }
+    };
+};
 
 // Action pour supprimer un remboursement
-// export const deleteRefund = (transactionId: any, refundId: any) => {
-//     return async (dispatch: any) => {
-//         try {
-//             await axios.delete(`http://localhost:5001/transactions/${transactionId}/refund/${refundId}`);
-//             dispatch({ type: DELETE_REFUND, payload: refundId });
-//             dispatch({ type: GET_TRANSACTIONS }); // Rafraîchir les transactions après la suppression du remboursement
-//         } catch (error) {
-//             throw error;
-//         }
-//     };
-// };
+export const deleteRefund = (transactionId: any, refundId: any) => {
+    return async (dispatch: any) => {
+        try {
+            await axios.delete(`http://localhost:5001/transactions/${transactionId}/refund/${refundId}`);
+            dispatch({ type: DELETE_REFUND, payload: refundId });
+            dispatch({ type: GET_TRANSACTIONS }); // Rafraîchir les transactions après la suppression du remboursement
+        } catch (error) {
+            throw error;
+        }
+    };
+};
--- src/redux/reducers/transaction.reducer.ts
+++ src/redux/reducers/transaction.reducer.ts
@@ -1,4 +1,5 @@
 import { GET_TRANSACTIONS, ADD_TRANSACTIONS, EDIT_TRANSACTIONS, DELETE_TRANSACTIONS } from "../actions/transaction.action";
+import { ADD_REFUND, EDIT_REFUND, DELETE_REFUND } from "../actions/refund.action";
 
 const initialState: any = [];
 
@@ -19,6 +20,37 @@ export default function transactionReducer(state = initialState, action: any) {
             });
         case DELETE_TRANSACTIONS:
             return state.filter((transaction: any) => transaction.id !== action.payload);
+        case ADD_REFUND:
+            return state.map((transaction: any) => {
+                if (transaction._id === action.payload._id) {
+                    return {
+                        ...transaction,
+                        remboursements: [...transaction.remboursements, action.payload.refund]
+                    };
+                } else return transaction;
+            });
+        case EDIT_REFUND:
+            return state.map((transaction: any) => {
+                if (transaction._id === action.payload.transactionId) {
+                    return {
+                        ...transaction,
+                        remboursements: transaction.remboursements.map((refund: any) =>
+                            refund._id === action.payload.refund._id ? action.payload.refund : refund
+                        ),
+                    };
+                } else return transaction;
+            });
+        case DELETE_REFUND:
+            return state.map((transaction: any) => {
+                if (transaction._id === action.payload.transactionId) {
+                    return {
+                        ...transaction,
+                        remboursements: transaction.remboursements.filter(
+                            (refund: any) => refund._id !== action.payload.refundId
+                        ),
+                    };
+                } else return transaction;
+            });
         default:
             return state;
     }
