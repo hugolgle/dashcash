1f0502d9ee65ac7dd51485eec4b614f773e637b0
Hugo Le Galle <hugo@Ordinateur-portable-de-Hugo.local>
2024-06-29 19:17:10 +0200

    commit

--- src/components/button/btnAdd.tsx
+++ src/components/button/btnAdd.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function BtnAdd(props: any) {
     return (
-        <Link className='cursor-pointer hover:scale-125 ease-in-out duration-300' to={props.to ? `${props.to}/add` : "add"}><CirclePlus className="hover:scale-125 ease-in-out duration-300" /></Link>
+        <Link className='cursor-pointer hover:scale-105 transition-all' to={props.to ? `${props.to}/add` : "add"}><CirclePlus className="hover:scale-125 ease-in-out duration-300" /></Link>
     )
 }
 
